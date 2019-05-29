254d6e752e0379be6f73e5e33a4aed3548c0dfe0
mark-marshall <mark.marshallgp@gmail.com>
2019-05-29 14:43:28 +0100

    Added Item class

---
+++ item.py
@@ -0,0 +1,8 @@
+# Implement a class to hold item information.
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"Item: {self.name}, Description: {self.description}"
