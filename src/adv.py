41e34329fc39a0104a9b9c55030d1edda0de1d3a
mark-marshall <mark.marshallgp@gmail.com>
2019-05-29 15:37:32 +0100

    Fixed conditional bug on user_selection

--- src/adv.py
+++ src/adv.py
@@ -2,40 +2,6 @@ from room import Room
 from  player import Player
 from item import Item
 
-# Declare all the rooms
-
-room = {
-    'outside':  Room("outside", "Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("foyer", "Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("overlook", "Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("narrow", "Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("treasure", "Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
-
-
-# Link rooms together
-
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
-
-
 # Declare all the items
 
 items = {
@@ -47,6 +13,37 @@ items = {
     'laptop':   Item("laptop", "hack the adv to be more fun"),
 }
 
+# Declare all the rooms
+
+room = {
+    'outside':  Room("outside", "Outside Cave Entrance",
+                     "North of you, the cave mount beckons", [items['sword']]),
+
+    'foyer':    Room("foyer", "Foyer", """Dim light filters in from the south. Dusty
+passages run north and east.""", []),
+
+    'overlook': Room("overlook", "Grand Overlook", """A steep cliff appears before you, falling
+into the darkness. Ahead to the north, a light flickers in
+the distance, but there is no way across the chasm.""", [items['laptop'], items['shield']]),
+
+    'narrow':   Room("narrow", "Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""", [items['peach'], items['banana']]),
+
+    'treasure': Room("treasure", "Treasure Chamber", """You've found the long-lost treasure
+chamber! Sadly, it has already been completely emptied by
+earlier adventurers. The only exit is to the south.""", [items['literature']]),
+}
+
+# Link rooms together
+
+room['outside'].n_to = room['foyer']
+room['foyer'].s_to = room['outside']
+room['foyer'].n_to = room['overlook']
+room['foyer'].e_to = room['narrow']
+room['overlook'].s_to = room['foyer']
+room['narrow'].w_to = room['foyer']
+room['narrow'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 player_selection = str(input("Type your player's name here: "))
@@ -68,18 +65,18 @@ while direction_selection != "q":
     print(room[player.location].name)
     # * Prints the current description (the textwrap module might be useful here).
     print(room[player.location].description)
-
     # Gets user input
-    direction_selection = str(input("Choose a direction to head: [n] North [e] East [s] South [w] West [q] Quit\n")).lower()
+    direction_selection = str(input("Choose a direction to head: [n] North [e] East [s] South [w] West [q] Quit\n"))
     # If the user enters a cardinal direction, attempts to move to the room there.
-    if direction_selection == "n" or "e" or "s" or "w":
+    if direction_selection == ("n" or "e" or "s" or "w"):
         if new_location_check(direction_selection, room[player.location]):
             player.location = new_location_check(direction_selection, room[player.location]).key
-        else:
-            print(f"{player.name} tries to head {direction_selection} and finds nothing. Try again!")
+            print(f"{player.name} heads {direction_selection} and enters {player.location}")
+        else: 
+            print(f"{player.name} tries to head {direction_selection} but doesn't find anything.")
     # If the user enters "q", quit the game.
     elif direction_selection == "q":
-            print(f"Thanks for playing {player.name}")
+        print(f"Thanks for playing {player.name}")
     # Print an error message if a non-valid key is entered
     else:
         print(f"{player.name} made an invalid selection, try again!")
--- src/room.py
+++ src/room.py
@@ -1,10 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, key, name, description):
+    def __init__(self, key, name, description, items):
         self.key = key
         self.name = name
         self.description = description
+        self.items = items
     
+    def __repr__(self):
+        return f"Room: {self.name}, Description: {self.description}, Items: {self.items}"
+
     def __str__(self):
-        return f"Room: {self.name}, Description: {self.description}"
+        return f"Room: {self.name}, Description: {self.description}, Items: {self.items}"
