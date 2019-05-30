46f1d7dc1750950e01b3d88c872afa1cef5bf54b
mark-marshall <mark.marshallgp@gmail.com>
2019-05-30 12:25:04 +0100

    Added on_take and on_drop methods on Item

--- src/item.py
+++ src/item.py
@@ -4,5 +4,11 @@ class Item:
         self.name = name
         self.description = description
 
+    def on_take(self):
+        return f"You picked up {self.name}: {self.description}"
+    
+    def on_drop(self):
+        return f"You dropped {self.name}"
+
     def __str__(self):
         return f"Item: {self.name}, Description: {self.description}"
